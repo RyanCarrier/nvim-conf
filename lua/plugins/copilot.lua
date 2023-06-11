9a15d0d7be59d76d9899d494af4dd7dbec035a0d
Ryan Carrier <ryan.j.carrier@gmail.com>
2023-06-11 18:16:46 +1000

    update init of copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,20 +3,20 @@ return
     -- zbirenbaum copilot-cmp is very cool
     "zbirenbaum/copilot.lua",
     event = "InsertEnter",
-    opts = {
-        suggestion = {
-            auto_trigger = true,
+    config = function()
+        require('copilot').setup({
+            suggestion = {
+                auto_trigger = true,
 
-            keymap = {
-                accept = "<M-j>",
-                accept_line = "<C-l>",
-                --  accept_word = "<M-k>",
-                next = "<C-]>",
-                prev = "<C-[>",
-                dismiss = "<M-h>",
+                keymap = {
+                    accept = "<M-j>",
+                    accept_line = "<C-l>",
+                    --  accept_word = "<M-k>",
+                    next = "<C-]>",
+                    prev = "<C-[>",
+                    dismiss = "<M-h>",
+                },
             },
-        },
-    },
-
-
+        })
+    end
 }
