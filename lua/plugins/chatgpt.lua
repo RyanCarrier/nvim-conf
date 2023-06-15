e7515032b2b36346a3478d0d0867f0273d941703
Ryan Carrier <ryan.j.carrier@gmail.com>
2023-06-15 14:08:44 +1000

    update with plenary use need full path

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -3,7 +3,7 @@ return {
     event = "VeryLazy",
     config = function()
         require("chatgpt").setup({
-            api_key_cmd = "cat ~/.chatgpt.api.key",
+            api_key_cmd = "cat /home/rcarrier/.chatgpt.api.key",
             -- chat = {
             --     keymaps = {
             --         submit = { "<C-m>", "<C-Enter>", "<C-t>" },
